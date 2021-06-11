7130f639d5c733c906e2208104d6fe74b6e88678
Sobiah Selvarajah <sobiahselvarajah@Sobiahs-MacBook-Pro.local>
2021-06-11 01:45:05 +0100

    server side - GET request single poem

--- server/routes/poems-simpler.js
+++ server/routes/poems-simpler.js
@@ -13,7 +13,8 @@ const writeData = () => {
     fs.writeFileSync('./data/poems.json', JSON.stringify(myData))
 };
 
-// GET request for list of poems
+
+//! GET request for list of poems
 router.get('/', (_req, res) => {
 
     const poemsData = readData();
@@ -29,10 +30,28 @@ router.get('/', (_req, res) => {
     })
     res.status(200)
         .json(poemsFiltered);
+
 });
 
 
+//! GET request for single poem
+router.get('/:poemId', (req, res) => {
 
+    const poemsData = readData();
+
+    const currentPoemId = req.params.poemId;
+
+    const currentPoem = poemsData.find( 
+        poem => poem.data.id === currentPoemId);
+    
+    if (!currentPoem)
+        res.status(404)
+            .json(`Sorry, the poem with id ${currentPoemId} cannot be found`);
+        
+    res.status(200)
+        .json(currentPoem);
+
+});
 
 
 
