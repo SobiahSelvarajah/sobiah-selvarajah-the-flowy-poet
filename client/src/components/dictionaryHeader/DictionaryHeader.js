552f4f463d568706f1d7fe416531a929e120edea
Sobiah Selvarajah <sobiahselvarajah@Sobiahs-MacBook-Pro.local>
2021-06-25 02:19:46 +0100

    client side - adding dictionaryHeader component

---
+++ client/src/components/dictionaryHeader/DictionaryHeader.js
@@ -0,0 +1,30 @@
+import { MenuItem, TextField } from '@material-ui/core';
+import React from 'react';
+
+import './DictionaryHeader.scss';
+
+const DictionaryHeader = () => {
+    return (
+        <div className="dictionaryHeader__container">
+            <span className="dictionaryHeader__heading">
+                Word Hunt
+            </span>
+            <div className="dictionaryHeader__inputs">
+                <TextField id="standard-basic" label="Standard" />
+                <TextField 
+                    id="standard-select-country"
+                    select
+                    label="Select"
+                    helperText="Please select your language"
+                >
+                    <MenuItem>
+                        English
+                    </MenuItem>
+                </TextField>
+            </div>
+            
+        </div>
+    )
+}
+
+export default DictionaryHeader
--- client/src/pages/dictionaryPage/DictionaryPage.js
+++ client/src/pages/dictionaryPage/DictionaryPage.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container } from '@material-ui/core';
+import DictionaryHeader from '../../components/dictionaryHeader/DictionaryHeader';
+import './DictionaryPage.scss';
 
 const DictionaryPage = () => {
+
+    const [word, setWord] = useState("");
+    const [meanings, setMeanings] = useState([]);
+
+    const dictionaryAPI = async () => {
+        try {
+            const data = await axios.get(
+                "https://api.dictionaryapi.dev/api/v2/entries/en/poet"
+            );
+
+            // console.log(data);
+            setMeanings(data.data)
+        } catch(error) {
+            console.log(error);
+        } 
+    };
+
+    console.log(meanings);
+
+    useEffect(() => {
+        dictionaryAPI();
+        
+    }, [])
+
+
     return (
-        <div>
+        <section className="dictionaryPage__section">
+            <Container 
+                maxWidth="md"
+                className="dictionaryPage__container">
+                    <DictionaryHeader />
+            </Container>
+            <div className="dictionaryPage__quote">
+                <p className="dictionaryPage__quote-text">
+                    "Impossible is a word to be found only in the dictionary of fools."
+                    <br/>
+                    -Napoleon Bonaparte
+                </p>
+            </div>
             
-        </div>
+        </section>
     )
 }
 
