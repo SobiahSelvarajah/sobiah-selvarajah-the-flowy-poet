d5ff1a3c5270e88a0b3736c53954631a11bf04ce
Sobiah Selvarajah <sobiahselvarajah@Sobiahs-MacBook-Pro.local>
2021-06-27 16:50:48 +0100

    client side - added hobbies into AboutMe

--- client/src/pages/aboutMe/AboutMe.js
+++ client/src/pages/aboutMe/AboutMe.js
@@ -14,14 +14,18 @@ const AboutMe = () => {
         <section className="aboutMe__container">
             <AboutMeHeader />
             <AboutMeBio />
+            <p className="aboutMe__subheading-hobbies">Digging into my hobbies</p>            
             <div className="aboutMe__video-container">
                 <VideoPlayer 
                     className="aboutMe__video"
                     src={videoSrc}
                 />
+                <span className="aboutMe__video-caption">
                 A snippet of me losing myself to the music...
                 <br />
+                <br />
                 Song: Ang Laga De
+                </span>
             </div>
         </section>
     )
